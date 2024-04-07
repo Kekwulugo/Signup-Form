0e3e261b603b626a7f4579995dc43d887a623ff7
Kelechi Ekwulugo <kekwulugo@gmail.com>
2024-04-06 19:06:20 -0600

    Add form validations with JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,8 @@ let submitBtn = document.querySelector("button");
 let passwordEl = document.querySelector(".password");
 let passwordMatchEl = document.querySelector(".password1");
 let feedbackEl = document.querySelector("#feedback");
+let emailEl = document.querySelector("#email");
+let phoneEl = document.querySelector("#phone");
 
 submitBtn.addEventListener("click", ()=>{
  console.log("clicked");
@@ -24,6 +26,29 @@ submitBtn.addEventListener("click", ()=>{
   passwordMatchEl.classList = "password";
 
 
+
+
  }
 
 })
+
+emailEl.addEventListener("input", (event) =>{
+ console.log(emailEl.validity.typeMismatch);
+ if(emailEl.validity.typeMismatch){
+  emailEl.setCustomValidity("I am expecting an email address!");
+  
+ } else {
+  emailEl.setCustomValidity("");
+ }
+});
+
+phoneEl.addEventListener('input', (event)=>{
+ console.log("typing phone");
+ console.log(phoneEl.validity.valid);
+ if (phoneEl.validity.tooShort){
+  phoneEl.setCustomValidity("Your phone number must be 10 characters");
+ }else{
+  phoneEl.setCustomValidity("");
+ }
+});
+
