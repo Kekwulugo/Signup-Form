306c77251739396f87ce7601fe39b1ce20b3c57b
Kelechi Ekwulugo <kekwulugo@gmail.com>
2024-04-08 20:22:54 -0600

    Update form validation and move button into form element

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,8 +2,11 @@ let submitBtn = document.querySelector("button");
 let passwordEl = document.querySelector(".password");
 let passwordMatchEl = document.querySelector(".password1");
 let feedbackEl = document.querySelector("#feedback");
+let matchEl = document.querySelector("#match");
 let emailEl = document.querySelector("#email");
 let phoneEl = document.querySelector("#phone");
+let accountEl = document.querySelector("#account");
+let rightContainer = document.querySelector(".right-container");
 
 submitBtn.addEventListener("click", ()=>{
  console.log("clicked");
@@ -17,13 +20,23 @@ submitBtn.addEventListener("click", ()=>{
  } else if (passwordEl.value !== passwordMatchEl.value) {
   passwordEl.classList = "error";
   passwordMatchEl.classList = "error";
-  feedbackEl.innerText = "*Password must match!"
+  feedbackEl.innerText = " *password must match!"
   
  } else{
   console.log("Passwords Match");
   feedbackEl.innerText = "*"
   passwordEl.classList = "password";
   passwordMatchEl.classList = "password";
+
+  let sucessMessage = document.createElement("p");
+  sucessMessage.setAttribute("id", "account");
+  sucessMessage.style.color = "green";
+  sucessMessage.innerText = "Congrats your response has been submitted!"
+  
+  submitBtn.remove();
+  accountEl.remove();
+  rightContainer.appendChild(sucessMessage);
+  
 
 
 
@@ -32,23 +45,19 @@ submitBtn.addEventListener("click", ()=>{
 
 })
 
-emailEl.addEventListener("input", (event) =>{
- console.log(emailEl.validity.typeMismatch);
- if(emailEl.validity.typeMismatch){
-  emailEl.setCustomValidity("I am expecting an email address!");
-  
- } else {
-  emailEl.setCustomValidity("");
- }
-});
+//live password validation
 
-phoneEl.addEventListener('input', (event)=>{
- console.log("typing phone");
- console.log(phoneEl.validity.valid);
- if (phoneEl.validity.tooShort){
-  phoneEl.setCustomValidity("Your phone number must be 10 characters");
- }else{
-  phoneEl.setCustomValidity("");
+passwordMatchEl.addEventListener('input',(event)=>{
+ if(passwordEl.value !== passwordMatchEl.value){
+  matchEl.innerHTML = " *password does not match";
+  passwordEl.classList = "error";
+  passwordMatchEl.classList = "error";
+
+ } else {
+  matchEl.innerHTML = "*";
+  passwordEl.classList = "password";
+  passwordMatchEl.classList = "password";
+
  }
-});
+})
 
